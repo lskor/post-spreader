47ab0c205ec227bb510ba2960cdf64439d70422e
lskor <glory.web3@gmail.com>
2024-01-04 00:07:09 +0200

    feat: basic bot

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,3 +1,13 @@
-class Dependancies {
-
+object Dependencies {
+  val fs2Version = "3.7.0"
+  val catsCoreVersion = "2.9.0"
+  val catsEffectVersion = "3.4.8"
+  val canoeVersion = "0.6.0"
+  val http4sVersion = "0.23.18"
+  val log4CatsVersion = "2.5.0"
+  val logbackClassic = "1.4.7"
+  val jsoup = "1.16.1"
+  val doobieVersion = "1.0.0-RC1"
+  val postgresql = "42.6.0"
+  val scalaTestVersion = "3.2.12"
 }
--- src/main/scala/SpreaderApp.scala
+++ src/main/scala/SpreaderApp.scala
@@ -1,3 +1,24 @@
-class SpreaderApp {
+import canoe.api._
+import canoe.syntax._
+import cats.effect.{IO, IOApp}
+import fs2.Stream
 
+object SpreaderApp extends IOApp.Simple {
+
+  private val token: String = "5824728765:AAF3IWKWvnp837E4-Ne_c_5t49RBpm__eag"
+
+  def run: IO[Unit] =
+    Stream
+      .resource(TelegramClient[IO](token))
+      .flatMap(implicit client => Bot.polling[IO].follow(greetings))
+      .compile
+      .drain
+
+  def greetings[F[_] : TelegramClient]: Scenario[F, Unit] =
+    for {
+        chat <- Scenario.expect(command("hi").chat)
+      _ <- Scenario.eval(chat.send("Hello. What's your name?"))
+      name <- Scenario.expect(text)
+      _ <- Scenario.eval(chat.send(s"Nice to meet you, $name"))
+    } yield ()
 }
